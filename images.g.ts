7d07faf5721791ffc3dfc9d4d739f350af77657b
KodeklubbenSalangen <112966813+KodeklubbenSalangen@users.noreply.github.com>
2022-09-20 17:40:58 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "flap_pee_bird_flap":return img`
+. . . . . . f f f f f f . . . . . 
+. . . . f f 5 5 5 f 1 1 f . . . . 
+. . . f 5 5 5 5 f 1 1 1 1 f . . . 
+. . f 5 5 5 5 5 f 1 1 1 f 1 f . . 
+. f f f f f 5 5 f 1 1 1 f 1 f . . 
+f 1 1 1 1 1 f 5 5 f 1 1 1 1 f . . 
+f d 1 1 1 d f 5 5 5 f f f f f f . 
+f f f f f f 5 5 5 f 2 2 2 2 2 2 f 
+. f 4 4 4 4 4 4 f 2 f f f f f f . 
+. . f 4 4 4 4 4 4 f 2 2 2 2 2 f . 
+. . . f f 4 4 4 4 4 f f f f f . . 
+. . . . . f f f f f . . . . . . . 
+`;
+            case "image1":
+            case "flap_pee_bird":return img`
+. . . . . . f f f f f f . . . . . 
+. . . . f f 5 5 5 f 1 1 f . . . . 
+. . . f 5 5 5 5 f 1 1 1 1 f . . . 
+. f f f f 5 5 5 f 1 1 1 f 1 f . . 
+f 1 1 1 1 f 5 5 f 1 1 1 f 1 f . . 
+f 1 1 1 1 1 f 5 5 f 1 1 1 1 f . . 
+f d 1 1 1 d f 5 5 5 f f f f f f . 
+. f d d d f 5 5 5 f 2 2 2 2 2 2 f 
+. . f f f 4 4 4 f 2 f f f f f f . 
+. . f 4 4 4 4 4 4 f 2 2 2 2 2 f . 
+. . . f f 4 4 4 4 4 f f f f f . . 
+. . . . . f f f f f . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . f f f f f f . . . . . 
+. . . . f f 5 5 5 f 1 1 f . . . . 
+. . . f 5 5 5 5 f 1 1 1 1 f . . . 
+. f f f f 5 5 5 f d 1 1 f 1 f . . 
+f 1 1 1 1 f 5 5 f d 1 1 f 1 f . . 
+f 1 1 1 1 1 f 5 5 f d 1 1 1 f . . 
+f d 1 1 1 d f 5 5 5 f f f f f . . 
+. f d d d f 5 5 5 f 2 2 2 2 2 f . 
+. . f f f 4 4 4 f 2 f f f f f . . 
+. . f 4 4 4 4 4 4 f 2 2 2 2 f . . 
+. . . f f 4 4 4 4 4 f f f f . . . 
+. . . . . f f f f f . . . . . . . 
+`, img`
+. . . . . . f f f f f f . . . . . 
+. . . . f f 5 5 5 f 1 1 f . . . . 
+. . . f 5 5 5 5 f 1 1 1 1 f . . . 
+. . f 5 5 5 5 5 f d 1 1 f 1 f . . 
+. f 5 5 5 5 5 5 f d 1 1 f 1 f . . 
+. f f f f f 5 5 5 f d 1 1 1 f . . 
+f 1 1 1 1 1 f 5 5 5 f f f f f . . 
+f d 1 1 1 d f 5 5 f 2 2 2 2 2 f . 
+. f f f f f 4 4 f 2 f f f f f . . 
+. . f 4 4 4 4 4 4 f 2 2 2 2 f . . 
+. . . f f 4 4 4 4 4 f f f f . . . 
+. . . . . f f f f f . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
